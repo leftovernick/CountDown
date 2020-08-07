40ccfdb9914a25bbde4219f422e56f96e0dd1c05
Nick Gray <NicholasGray@apple.com>
2020-08-07 14:11:39 -0700

    Initial Commit

---
+++ CountDown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountDown
+//
+//  Created by Nick Gray on 8/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
